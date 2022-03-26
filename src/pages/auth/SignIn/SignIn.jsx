3674b8a150421d813e57a2e38aa214b9c5afd893
pankaj-prog <wadhwanip38@gmail.com>
2022-03-26 11:46:42 +0530

    feat: auth pages - signin & signup

--- src/pages/auth/SignIn/SignIn.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SignIn = () => {
-  return <h1>This is Sign in page</h1>;
-};
-
-export default SignIn;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ export { default as Cart } from "./Cart/Cart";
 export { default as Home } from "./Home/Home";
 export { default as Wishlist } from "./Wishlist/Wishlist";
 export { default as Courses } from "./Courses/Courses";
-export { default as SignIn } from "./auth/SignIn/SignIn";
+export { default as SignIn } from "./auth/SignIn";
+export { default as SignUp } from "./auth/SignUp";
 export { default as CourseDetails } from "./CourseDetails/CourseDetails";
